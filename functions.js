4cddbcb6f0d4d7e44727bae207bdc34837a67e33
chase <chasepark331@gmail.com>
2021-01-11 11:33:19 +0900

    ..

--- functions.js
+++ functions.js
@@ -1,24 +1,27 @@
 
 var body = {
     setColor:function(color){
-        var target = document.querySelector('body');
-        target.style.color=color;
+        // var target = document.querySelector('body');
+        // target.style.color=color;
+        $('body').css('color',color);
     },
     setBackgroundColor:function(color){
-        var target = document.querySelector('body');
-        target.style.backgroundColor=color;
+        // var target = document.querySelector('body');
+        // target.style.backgroundColor=color;
+        $('body').css('backgroundColor',color);
     }   
 }
 
 
 var links = {
     setColor:function(color){
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i<alist.length){
-            alist[i].style.color = color;
-            i=i+1; 
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i<alist.length){
+        //     alist[i].style.color = color;
+        //     i=i+1; 
+        // }
+        $('a').css('color', color);
     }
 }
 
